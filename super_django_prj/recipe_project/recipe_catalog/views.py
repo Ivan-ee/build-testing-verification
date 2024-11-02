68fc495d3d9d3bb2c39045cd85ef115b14f2a914
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-02 10:46:58 +0300

    register app recipe_catalog

--- super_django_prj/recipe_project/recipe_catalog/views.py
+++ super_django_prj/recipe_project/recipe_catalog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    return HttpResponse("О проекте")
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def recipe_detail(request, pk):
+    return HttpResponse(f'Описание проекта id = {pk}')
