80c100f0a4e7a6e44d042b0905e0c5139b1e96ab
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-03 11:57:00 +0300

    update recipe_detail

--- super_django_prj/recipe_project/recipe_catalog/views.py
+++ super_django_prj/recipe_project/recipe_catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Recipe
+
 
 def about(request):
     template_name = 'recipe_catalog/about.html'
@@ -17,7 +19,17 @@ def index(request):
 def recipe_detail(request, pk):
     template_name = 'recipe_catalog/recipe.html'
 
-    title = 'Блинчики с мясом'
-    context = {'title': title, 'recipe_id': pk}
+    # title = 'Блинчики с мясом'
+    # context = {'title': title, 'recipe_id': pk}
+    #
+    # return render(request, template_name, context)
+
+    recipe = Recipe.objects.get(pk=pk)
+    context = {
+        'title': recipe.name,
+        'recipe_id': pk,
+        'ingredients': recipe.ingredients.order_by('name')
+    }
 
     return render(request, template_name, context)
+
