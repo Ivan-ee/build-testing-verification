909652a8923ef72e614955ba20779fffbbd2d1b2
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-03 10:55:54 +0300

    update admin models

--- super_django_prj/recipe_project/recipe_catalog/admin.py
+++ super_django_prj/recipe_project/recipe_catalog/admin.py
@@ -2,7 +2,30 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Ingredient, Recipe
+from .models import Ingredient, Recipe, RecipeIngredient
 
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
+# admin.site.register(Ingredient)
+# admin.site.register(Recipe)
+
+
+class IngredientInline(admin.StackedInline):
+    """В рецепте есть ингредиенты."""
+    model = RecipeIngredient
+    extra = 5
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    """Настройка формы админки для рецепта."""
+    inlines = [IngredientInline]
+    list_display = ["name"]
+
+
+admin.site.register(Recipe, RecipeAdmin)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    """Настройка формы админки для рецепта."""
+    list_display = ["name"]
+
+
+admin.site.register(Ingredient, IngredientAdmin)
