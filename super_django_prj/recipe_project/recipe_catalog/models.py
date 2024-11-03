412cb8ba8fc8c1901987bd5f04ec404cf7ccc4d7
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-03 10:32:54 +0300

    add new models

--- super_django_prj/recipe_project/recipe_catalog/models.py
+++ super_django_prj/recipe_project/recipe_catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Ingredient(models.Model):
+    """Составная часть рецепта."""
+    name = models.CharField(max_length=255)
+
+    class Recipe(models.Model):
+        """Вкусное делается по рецепту."""
+        name = models.CharField(max_length=300)
