5de9f13556fa6ec4b95d242dec53ba5a1adc7a9a
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-03 10:41:49 +0300

    add RecipeIngredient model

--- super_django_prj/recipe_project/recipe_catalog/models.py
+++ super_django_prj/recipe_project/recipe_catalog/models.py
@@ -11,3 +11,13 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     """Вкусное делается по рецепту."""
     name = models.CharField(max_length=300)
+    ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
+
+
+class RecipeIngredient(models.Model):
+    """Один ингредиент может быть
+    в нескольких рецептах,
+    как и в одном рецепте может быть
+    несколько ингредиентов."""
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
