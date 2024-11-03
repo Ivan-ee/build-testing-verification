d797d64de6a588e1463a24919a5769d2e0092de3
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-03 10:38:47 +0300

    register models in admin

--- super_django_prj/recipe_project/recipe_catalog/models.py
+++ super_django_prj/recipe_project/recipe_catalog/models.py
@@ -7,6 +7,7 @@ class Ingredient(models.Model):
     """Составная часть рецепта."""
     name = models.CharField(max_length=255)
 
-    class Recipe(models.Model):
-        """Вкусное делается по рецепту."""
-        name = models.CharField(max_length=300)
+
+class Recipe(models.Model):
+    """Вкусное делается по рецепту."""
+    name = models.CharField(max_length=300)
