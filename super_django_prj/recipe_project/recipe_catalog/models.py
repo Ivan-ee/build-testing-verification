a7dc6fca9dc0491f222ebf499f1c696dd5bf74a5
Ivan-ee <smirnowwwivan@mail.ru>
2024-11-26 23:43:00 +0300

    update model Ingredient

--- super_django_prj/recipe_project/recipe_catalog/models.py
+++ super_django_prj/recipe_project/recipe_catalog/models.py
@@ -4,9 +4,10 @@ from django.utils.html import mark_safe
 from django.db import models
 
 
-class Ingredient(models.Model):
-    """Составная часть рецепта."""
+from django.core.exceptions import ValidationError
 
+
+class Ingredient(models.Model):
     MEASUREMENT_SCALE = [
         ('g', 'Граммы'),
         ('pcs', 'Штуки'),
@@ -17,37 +18,60 @@ class Ingredient(models.Model):
 
     name = models.CharField(max_length=255, verbose_name="Название")
     image = models.ImageField(upload_to="images/", default="images/default.jpg", verbose_name="Изображение")
-    calories = models.DecimalField(
-        default=0.0,
-        max_digits=6,
-        decimal_places=2,
-        help_text="Калорийность на 100 ед/изм",
-        verbose_name="Калорийность",
-    )
+
     unit = models.CharField(
         max_length=10,
         choices=MEASUREMENT_SCALE,
         default='g',
         verbose_name="Единица измерения",
     )
+    conversion_to_grams = models.IntegerField(
+        default=0,
+        null=True,
+        blank=True,
+        help_text="Если ед. изм. штуки, то указать вес на 1 штуку в граммах",
+        verbose_name="Вес шт/гр (опционально)",
+    )
+
+    calories = models.DecimalField(
+        default=0.0,
+        max_digits=6,
+        decimal_places=2,
+        help_text="Калорийность на 100 ед. изм.",
+        verbose_name="Калорийность",
+    )
+
+    def clean(self):
+        """Валидация данных модели."""
+        if self.unit == 'pcs' and (self.conversion_to_grams is None or self.conversion_to_grams <= 0):
+            raise ValidationError({
+                'conversion_to_grams': "Для единицы измерения 'штуки' необходимо указать вес в граммах."
+            })
+        if self.unit != 'pcs' and self.conversion_to_grams:
+            raise ValidationError({
+                'conversion_to_grams': "Поле 'Вес шт/гр' должно быть пустым для выбранной единицы измерения."
+            })
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
-    """Вкусное делается по рецепту."""
     name = models.CharField(max_length=300)
     description = models.TextField(default="", help_text="Описание")
     cooking_time = models.IntegerField(default=0)
     ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
 
     def total_weight(self):
-        return sum(ri.weight for ri in self.recipeingredient_set.all())
+        return sum(
+            ri.weight * ri.ingredient.conversion_to_grams
+            for ri in self.recipeingredient_set.all()
+        )
 
     def total_calories(self):
         total_calories = sum(
-            (ri.weight / 100) * ri.ingredient.calories for ri in self.recipeingredient_set.all()
+            (ri.weight * ri.ingredient.conversion_to_grams / 100) * ri.ingredient.calories
+            for ri in self.recipeingredient_set.all()
         )
         return round(total_calories, 2)
 
