35f8faab4b25e9f1e418f049f668a47843b14f1a
Ivan-ee <smirnowwwivan@mail.ru>
2024-12-01 21:59:36 +0300

    update view

--- super_django_prj/recipe_project/recipe_catalog/views.py
+++ super_django_prj/recipe_project/recipe_catalog/views.py
@@ -25,13 +25,26 @@ def recipe_detail(request, pk):
     template_name = 'recipe_catalog/recipe.html'
     recipe = get_object_or_404(Recipe, pk=pk)
 
+    ingredients = []
+    for ri in recipe.recipeingredient_set.select_related('ingredient').order_by('ingredient__name'):
+        ingredient = ri.ingredient
+        if ingredient.unit and ingredient.unit.key == 'pcs':
+            weight = ri.count * ingredient.weight_by_pcs
+            unit = f"{ri.count} шт"
+        else:
+            unit = f"{ri.count} {ingredient.unit.label}" if ingredient.unit else f"{ri.count} г"
+        ingredients.append({
+            'name': ingredient.name,
+            'unit': unit
+        })
+
     context = {
         'title': recipe.name,
         'description': recipe.description,
         'cooking_time': recipe.cooking_time,
         'total_weight': recipe.total_weight(),
         'total_calories': recipe.total_calories(),
-        'ingredients': recipe.recipeingredient_set.select_related('ingredient').order_by('ingredient__name')
+        'ingredients': ingredients,
     }
 
     return render(request, template_name, context)
