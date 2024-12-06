3e2f37920fd1f31b7a6aa3e5ce0046690221c1e6
Ivan-ee <smirnowwwivan@mail.ru>
2024-12-06 13:22:04 +0300

    add tests model

--- super_django_prj/recipe_project/recipe_catalog/tests/test_models.py
+++ super_django_prj/recipe_project/recipe_catalog/tests/test_models.py
@@ -1,33 +1,67 @@
-from django.test import TestCase
 from django.core.exceptions import ValidationError
-from django.db import IntegrityError
+from django.test import TestCase
+from django.db import IntegrityError, transaction
 from ..models import MeasurementScale, VolumeUnitConversion, Ingredient, Recipe, RecipeIngredient, User
 
 
 class TestMeasurementScaleModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.scale = MeasurementScale.objects.create(label="msl", key="msk", abbreviation="msabr")
+
     def test_measurement_scale_creation(self):
-        scale = MeasurementScale.objects.create(label="граммы", key="g", abbreviation="гр.")
-        self.assertEqual(scale.label, "граммы")
-        self.assertEqual(scale.key, "g")
-        self.assertEqual(scale.abbreviation, "гр.")
+        self.assertEqual(self.scale.label, "msl")
+        self.assertEqual(self.scale.key, "msk")
+        self.assertEqual(self.scale.abbreviation, "msabr")
+
+    def test_measurement_scale_unique(self):
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                MeasurementScale.objects.create(label="msl", key="msk2", abbreviation="msabr")
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                MeasurementScale.objects.create(label="msl2", key="msk", abbreviation="msabr")
 
 
 class TestVolumeUnitConversionModel(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.scale = MeasurementScale.objects.create(label="стакан", key="cup", abbreviation="ст.")
+        cls.conversion = VolumeUnitConversion.objects.create(unit=cls.scale, weight=250)
 
     def test_volume_unit_conversion_creation(self):
-        conversion = VolumeUnitConversion.objects.create(unit=self.scale, weight=250)
-        self.assertEqual(conversion.unit, self.scale)
-        self.assertEqual(conversion.weight, 250)
+        self.assertEqual(self.conversion.unit, self.scale)
+        self.assertEqual(self.conversion.weight, 250)
+
+    def test_volume_unit_conversion_with_none_scale(self):
+        with self.assertRaises(IntegrityError):
+            VolumeUnitConversion.objects.create(unit=None, weight=None)
+
+    def test_volume_unit_conversion_unique(self):
+        with self.assertRaises(IntegrityError):
+            MeasurementScale.objects.create(label="стакан", key="cup", abbreviation="ст.")
 
 
 class TestIngredientModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.ingredient = Ingredient.objects.create(name="Мука", calories=350)
+
     def test_ingredient_creation(self):
-        ingredient = Ingredient.objects.create(name="Мука", calories=350)
-        self.assertEqual(ingredient.name, "Мука")
-        self.assertEqual(ingredient.calories, 350)
+        self.assertEqual(self.ingredient.name, "Мука")
+        self.assertEqual(self.ingredient.calories, 350)
+
+    def test_ingredient_unique(self):
+        with self.assertRaises(IntegrityError):
+            Ingredient.objects.create(name="Мука")
+
+    def test_ingredient_without_name(self):
+        ingredient = Ingredient.objects.create()
+
+        self.assertEqual(ingredient.name, "Ингредиент")
 
 
 class TestRecipeModel(TestCase):
@@ -36,29 +70,45 @@ class TestRecipeModel(TestCase):
         cls.author = User.objects.create(username="test_user")
         cls.recipe = Recipe.objects.create(
             name="Блины",
-            description="Традиционные русские блины.",
+            description="Традиционные русские блины",
             cooking_time=30,
             author=cls.author,
         )
         cls.ingredient1 = Ingredient.objects.create(name="Мука", calories=350)
         cls.ingredient2 = Ingredient.objects.create(name="Молоко", calories=50)
-        cls.scale = MeasurementScale.objects.create(label="граммы", key="g", abbreviation="гр.")
+        cls.scale = MeasurementScale.objects.get(key="g")
+
+        RecipeIngredient.objects.create(recipe=cls.recipe, ingredient=cls.ingredient1, unit=cls.scale, count=200)
+        RecipeIngredient.objects.create(recipe=cls.recipe, ingredient=cls.ingredient2, unit=cls.scale, count=500)
 
     def test_recipe_creation(self):
         self.assertEqual(self.recipe.name, "Блины")
         self.assertEqual(self.recipe.author, self.author)
-        self.assertEqual(self.recipe.description, "Традиционные русские блины.")
+        self.assertEqual(self.recipe.description, "Традиционные русские блины")
         self.assertEqual(self.recipe.cooking_time, 30)
 
     def test_total_weight_calculation(self):
-        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient1, unit=self.scale, count=200)
-        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient2, unit=self.scale, count=500)
         self.assertEqual(self.recipe.total_weight(), 700)
 
     def test_total_calories_calculation(self):
-        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient1, unit=self.scale, count=200)
-        RecipeIngredient.objects.create(recipe=self.recipe, ingredient=self.ingredient2, unit=self.scale, count=500)
-        self.assertEqual(self.recipe.total_calories(), 300)
+        self.assertEqual(self.recipe.total_calories(), 950)
+
+    def test_recipe_creation_with_duplicate_name(self):
+        with self.assertRaises(IntegrityError):
+            Recipe.objects.create(
+                name="Блины",
+                description="Те же самые блины",
+                cooking_time=20,
+                author=self.author,
+            )
+
+    def test_recipe_creation_without_author(self):
+        with self.assertRaises(IntegrityError):
+            Recipe.objects.create(
+                name="Рецепт",
+                description="Традиционные русские блины",
+                cooking_time=30,
+            )
 
 
 class TestRecipeIngredientModel(TestCase):
@@ -66,7 +116,7 @@ class TestRecipeIngredientModel(TestCase):
     def setUpTestData(cls):
         cls.recipe = Recipe.objects.create(name="Пирог", author=User.objects.create(username="test_user"))
         cls.ingredient = Ingredient.objects.create(name="Сахар", calories=400)
-        cls.unit = MeasurementScale.objects.create(label="штуки", key="pcs", abbreviation="шт.")
+        cls.unit = MeasurementScale.objects.get(key="pcs")
 
     def test_recipe_ingredient_creation(self):
         recipe_ingredient = RecipeIngredient.objects.create(
