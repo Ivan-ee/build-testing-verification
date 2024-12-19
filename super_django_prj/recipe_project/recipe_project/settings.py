2c5bc2a6c2e56ddac1ef55ff68136a6c0ecc3d57
Ivan-ee <smirnowwwivan@mail.ru>
2024-12-20 00:51:15 +0300

    set russian language

--- super_django_prj/recipe_project/recipe_project/settings.py
+++ super_django_prj/recipe_project/recipe_project/settings.py
@@ -101,13 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'ru'
 USE_I18N = True
-
-USE_TZ = True
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -127,3 +123,4 @@ OBJS_ON_PAGE = 6
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'auth/login/'
 LOGOUT_REDIRECT_URL = '/'
+
